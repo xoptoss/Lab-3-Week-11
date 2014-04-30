1b35c0e6887325a8a15bc02f4f3aa46a65fd1cb2
James Perez <xoptoss@gmail.com>
2014-04-29 20:16:45 -0700

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -5,9 +5,9 @@ public class mainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		dice Dice = new dice(3);
+		dice Dice = new dice(7);
 		
-		Dice.Throw(3);
+		Dice.Throw();
 		Dice.Value();
 		System.out.println(Dice.value);
 		
